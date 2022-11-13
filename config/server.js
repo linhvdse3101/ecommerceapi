5a74b16911abc09705aff919e9ac9ce0027b5887
linhvdse3101 <51112577+linhvdse3101@users.noreply.github.com>
2022-11-13 22:20:00 +0700

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "ecommerceapi-kz2w"),
-  port: env.int("PORT", 10000),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "828811e8aace1aa789067dffc85af376"),
