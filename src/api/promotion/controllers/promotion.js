077248bbe4e2c6e67920f4ff8d74459238def2d9
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-11-23 15:54:30 +0700

    delete priject

--- src/api/promotion/controllers/promotion.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * promotion controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::promotion.promotion');
--- src/api/promotion/routes/promotion.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * promotion router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::promotion.promotion');
