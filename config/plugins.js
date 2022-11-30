3b7d6ccd09aa63fdaa52f08f12f295f8e7e6db7e
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-11-30 17:55:06 +0700

    add plugins

---
+++ config/plugins.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          // endpoint: env('SCALEWAY_ENDPOINT'), // e.g. "s3.fr-par.scw.cloud"
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
