3537133fad48e9c7c10a19a51175c93c8b7c66cc
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-12-20 09:24:27 +0700

    nn

--- config/server.js
+++ config/server.js
@@ -2,6 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array(['API_TOKEN_SALT','ADMIN_JWT_SECRET','DATABASE_FILENAME','JWT_SECRET']),
+    keys: env.array('APP_KEYS',['ADMIN_JWT_SECRET','API_TOKEN_SALT','AWS_ACCESS_KEY_ID', 'AWS_ACCESS_SECRET', 'AWS_BUCKET',
+  'AWS_REGION','DATABASE_FILENAME', 'JWT_SECRET','HOST','PORT','SCALEWAY_ENDPOINT'
+  ]),
   },
 });
