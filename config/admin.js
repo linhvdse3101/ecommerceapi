8aab74d9c89cc4d88eef40e0a81644d556868c60
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-12-20 10:17:06 +0700

    ooooooo

--- config/admin.js
+++ config/admin.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
     'DATABASE_FILENAME','JWT_SECRET', 'NODE_ENV', 'SCALEWAY_ENDPOINT', 'SSH-RSA'),
   },
   apiToken: {
-    salt:env('JWT_SECRET'),
+    salt:env('ADMIN_JWT_SECRET','API_TOKEN_SALT','APP_KEYS','AWS_ACCESS_KEY_ID','AWS_ACCESS_SECRET','AWS_BUCKET','AWS_REGION',
+    'DATABASE_FILENAME','JWT_SECRET', 'NODE_ENV', 'SCALEWAY_ENDPOINT', 'SSH-RSA'),
   },
 });
