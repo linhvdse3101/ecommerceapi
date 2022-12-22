d9ada676437bcaa7156fc832b5f32bf63850cc3a
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-12-22 18:24:20 +0700

    hkhjkhjkhj

--- config/database.js
+++ config/database.js
@@ -8,4 +8,19 @@ module.exports = ({ env }) => ({
     },
     useNullAsDefault: true,
   },
+  // connection: {
+  //   client: 'postgres',
+  //   connection: {
+  //     host: env('DATABASE_HOST', 'localhost'),
+  //     port: env.int('DATABASE_PORT', 5432),
+  //     database: env('DATABASE_NAME', 'bank'),
+  //     user: env('DATABASE_USERNAME', 'postgres'),
+  //     password: env('DATABASE_PASSWORD', '0000'),
+  //     schema: env('DATABASE_SCHEMA', 'public'), // Not required
+  //     ssl: {
+  //       rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+  //     },
+  //   },
+  //   debug: false,
+  // },
 });
